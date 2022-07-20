44f0b74e585836b0a26f875a5d70594112ab20a4
Vili Vilenius <vili.vilenius@outlook.com>
2022-07-20 13:10:06 +0300

    Braket colour changes and a couple new highlightings

--- examples/example.py
+++ examples/example.py
@@ -1,11 +1,12 @@
-"""Example pythojn script"""
+"""Example python script"""
 import unittest
 from dataclasses import dataclass
 def thing(*args, kwarg = 1, another_kwarg = ('hmm', None)
           ) -> tuple[list[str], int]:
-    
-    return [another_kwarg[0]], None
 
+    return [another_kwarg[0]], None
+# A comment
+(((((())))))
 @dataclass
 class Test(unittest.case):
 
